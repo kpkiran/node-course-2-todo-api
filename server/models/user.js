7cd21d156be7266f33050491acbe72ca9eaf42d9
Ravi Kiran <Ravi Kiran>
2017-05-24 22:07:57 +1000

    added user model changes

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,35 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
 
 var User = mongoose.model('Users', {
     email: {
         type: String,
         required: true,
         minlength: 1,
-        trim: true
-    }
+        trim: true,
+        unique: true,
+        validate: {
+            validator: (value) => {
+                return validator.isEmail(value);
+            },
+            message: '{VALUE} is not a valid email address'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            require: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
 module.exports = { User };
--- server/server.js
+++ server/server.js
@@ -92,6 +92,19 @@ app.patch('/todos/:id', (req, res) => {
     })
 });
 
+// Users
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then(() => {
+        res.send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+});
+
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
